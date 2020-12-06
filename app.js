2d08dd8a78d380abe1e069e3a8696ff672472c83
Neeraj <asaini7788@gmail.com>
2020-12-05 21:56:40 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+var items = [];
+
+app.get("/" , function(req, res)
+{
+  var today = new Date();
+  var options = {
+    weekday : "long",
+    day : "numeric",
+    month : "long"
+  }
+  var day = today.toLocaleDateString("en-US", options);
+  res.render('index' , {kindofday:day , itemnames : items});
+});
+
+
+app.post("/", function(req , res)
+{
+  items.push(req.body.newitem);
+  res.redirect("/");
+});
+
+app.listen(3000 || process.env.PORT , function(req,res)
+{
+  console.log("server started");
+});
