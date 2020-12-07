cc715cd542812b7792d0fdd75d252d4dd6c38d71
Neeraj <asaini7788@gmail.com>
2020-12-06 21:23:40 -0500

    Work List Added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 var items = [];
+var workItems = [];
 
 app.get("/" , function(req, res)
 {
@@ -15,15 +16,49 @@ app.get("/" , function(req, res)
     month : "long"
   }
   var day = today.toLocaleDateString("en-US", options);
-  res.render('index' , {kindofday:day , itemnames : items});
+  res.render('index' , {listTitle:day , itemnames : items});
 });
 
 
 app.post("/", function(req , res)
 {
-  items.push(req.body.newitem);
-  res.redirect("/");
+  var listName = req.body.button;
+  console.log(req.body);
+  if(listName==="Work")
+  {
+    workItems.push(req.body.newitem);
+    res.redirect("/work");
+
+  }
+  else {
+    items.push(req.body.newitem);
+    res.redirect("/");
+  }
+
 });
+
+
+
+app.get("/about", function(req , res)
+{
+  res.render('about');
+});
+
+
+app.get("/work", function(req , res)
+{
+  res.render('index' , {listTitle: "Work List" , itemnames:workItems})
+});
+
+
+
+app.post("/work" , function(req , res)
+{
+    workItems.push(req.body.newitem);
+    res.redirect("/work");
+});
+
+
 
 app.listen(process.env.PORT || 3000 , function(req,res)
 {
